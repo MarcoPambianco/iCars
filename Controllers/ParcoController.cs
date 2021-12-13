e086124d1607df20ff0381da595175209ed110e9
NTSJ MarPam <marco.pambianco@ntsj.it>
2021-12-13 17:07:09 +0100

    Integrato servizio di loggind sul controller

--- Controllers/ParcoController.cs
+++ Controllers/ParcoController.cs
@@ -3,28 +3,32 @@ using System.Threading.Tasks;
 using iCars.Models.Interfaces;
 using iCars.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace iCars.Controllers
 {
     public class ParcoController : Controller
     {
         private readonly IParcoService parcoService;
-        public ParcoController(IParcoService parcoService)
+        private readonly ILogger logger;
+        public ParcoController(IParcoService parcoService, ILogger logger)
         {
+            this.logger = logger;
             this.parcoService = parcoService;
         }
         public async Task<IActionResult> Index()
         {
             ViewBag.Title = "Parco macchine";
-            
+
             // leggo l'elenco delle macchine del parco
             List<CarViewModel> lsAuto = await parcoService.GetParcoMacchine();
 
             return View(lsAuto);
         }
 
-        public async Task<IActionResult> Interventi(string strTarga) {
-            
+        public async Task<IActionResult> Interventi(string strTarga)
+        {
+
             // leggo i dettagli dell'auto e i relativi interventi
             CarDetailsViewModel carDetails = await parcoService.GetDettagliMacchinaAsync(strTarga);
             return View(carDetails);
